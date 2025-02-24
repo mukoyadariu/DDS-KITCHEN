07481c9cf22bdf34e1481efe13e1ad90043286b3
mukoyadariu <dennisdarius43@gmail.com>
2025-02-24 22:29:10 +0300

    today's special section

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ menuBtn.addEventListener("click",function(){
 closeBtn.addEventListener("click",function(){
     menus.classList.remove("active");
 })
+
+// swiper js code
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
